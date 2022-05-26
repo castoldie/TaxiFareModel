c00b2cdf915d745e200e9e9137f3604ddfc6325a
Edmondo Castoldi <edmondo.castoldi@protonmail.com>
2022-05-26 11:30:37 +0200

    merged all parts

---
+++ TaxiFareModel/params.py
@@ -0,0 +1,8 @@
+MLFLOW_URI = "https://mlflow.lewagon.ai/"
+EXPERIMENT_NAME = "[DE] [Berlin] [castoldie] tfm_v1"
+PATH_TO_LOCAL_MODEL = 'model.joblib'
+AWS_BUCKET_TEST_PATH = "s3://wagon-public-datasets/taxi-fare-test.csv"
+BUCKET_NAME = 'XXX'
+BUCKET_TRAIN_DATA_PATH = 'data/train_1k.csv'
+MODEL_NAME = 'taxifare'
+MODEL_VERSION = 'v2'
